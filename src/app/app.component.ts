3e655e3367a0ed25a3e89b0d5165f70fe3df33f7
Richardd82 <richardd82@gmail.com>
2023-10-03 17:30:39 -0600

    Get images from api - OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'counter-angular';
+  counter = 0;
+  photos: any[] = [];
+
+  increment(){
+    this.counter++;
+  }
+  decrement(){
+    this.counter === 0 ? null : this.counter--;
+  }
+  constructor(private http: HttpClient){
+   this.fetchPhotos();
+  }
+
+  ngOnInit(): void {
+    this.fetchPhotos();
+  }
+
+  fetchPhotos(): void{
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/photos')
+    .subscribe(data => {
+      this.photos = data
+    }, error => {
+      console.log('Error al obtener las fotos: ', error)
+    })
+  }
+
 }
